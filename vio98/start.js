5aea4bfd17187d7f4f588f34c01325c3d6166440
isadora88 <iwgajam@gmail.com>
2025-06-15 05:10:35 +0200

    das

--- vio98/start.js
+++ vio98/start.js
@@ -1,8 +1,33 @@
-onload = start; VERBOSE = true; TESTING = true; const SERVERDIR = 'vio98';
+onload = start; VERBOSE = true; TESTING = true;
 
 function start() { test0(); }
 
 async function test0() {
+	let loc = window.location.href; console.log('href', loc);
+	DA.isMoxito = loc.includes('moxito.online');
+	DA.isLocal = !DA.isMoxito && !loc.includes('telecave');
+	DA.isLive = !loc.includes('localhost');
+	DA.project = stringAfterLast(stringBeforeLast(loc, '/'), '/'); console.log('project', DA.project);
+	DA.staticUrl = DA.isLive ? '../' : 'https://moxito.online/';
+	DA.phpUrl = (DA.isLocal ? 'http://localhost:8080/mox/' : 'https://moxito.online/') + DA.project + '/php/';
+	DA.flaskUrl = (DA.isLocal ? 'http://localhost:5000/' : 'https://moxito.online/flaskgame0/');
+	DA.nodeUrl = (DA.isLocal ? 'http://localhost:3000/' : 'https://games.moxito.online/');
+	try {
+		let flaskLocal = await fetch(DA.flaskUrl); //, { mode: 'no-cors' })
+		console.log('flaskLocal', flaskLocal)
+	} catch { DA.flaskUrl = 'https://moxito.online/flaskgame0/' }
+	try {
+		let nodeLocal = await fetch(DA.nodeUrl); //, { mode: 'no-cors' })
+		console.log('flaskLocal', flaskLocal)
+	} catch { DA.nodeUrl = 'https://games.moxito.online/' }
+	//soll ich es so machen dass wenn local ist und local flask nicht laeuft, remote flask teste?
+
+
+	showObject(DA, null, 'dPage');
+	//DA.flaskUrl = 
+
+
+	return;
 
 	let url = 'https://moxito.online/flaskgame0';
 	let res = await fetch(url); //, { mode: 'no-cors' });
@@ -11,9 +36,9 @@ async function test0() {
 	} else {
 		console.log('Flask server is not running at', url);
 	}
-	let flask= await isServerRunning('flask', true);console.log('isServerRunning flask', flask);
+	let flask = await isServerRunning('flask', true); console.log('isServerRunning flask', flask);
 
-	let server = getServer();console.log('server',server)
+	let server = getServer(); console.log('server', server)
 	await loadAssetsStatic(); console.log(M)
 	API_BASE = getBackendUrl();
 
